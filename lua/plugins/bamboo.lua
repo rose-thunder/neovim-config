5926598f36191a60ee6ad65557654d54019d6ccd
Elli Cano <melongem64@gmail.com>
2025-04-18 13:37:09 -0700

    formatted scripts

--- lua/plugins/bamboo.lua
+++ lua/plugins/bamboo.lua
@@ -1,12 +1,12 @@
 return {
-  "ribru17/bamboo.nvim",
-  lazy = false,
-  priotity = 1000,
-  config = function()
-    local config = require("bamboo")
-    config.setup({
-      transparent = true,
-    })
-    vim.cmd("colorscheme bamboo")
-  end,
+	"ribru17/bamboo.nvim",
+	lazy = false,
+	priotity = 1000,
+	config = function()
+		local config = require("bamboo")
+		config.setup({
+			transparent = true,
+		})
+		vim.cmd("colorscheme bamboo")
+	end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,18 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  config = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    require("which-key").setup({})
-  end,
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = true })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+		require("which-key").setup({})
+	end,
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = true })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
