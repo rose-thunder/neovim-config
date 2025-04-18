e12468221bce3579ee469bd8414a2b8d0999b994
Elli Cano <melongem64@gmail.com>
2025-04-18 15:08:13 -0700

    added native neovim lsp support

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"html",
+					"cssls",
+					"pylsp",
+					"rust_analyzer",
+				},
+			})
+		end,
+	},
+}
