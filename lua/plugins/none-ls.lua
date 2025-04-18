dc6025141cb691d66bb80f6bf64c74a99bbb7359
Elli Cano <melongem64@gmail.com>
2025-04-18 10:56:25 -0700

    added more installed lsps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,6 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
-      debug = true,
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
