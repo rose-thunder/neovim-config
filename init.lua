9b53c400287facf6d7b7c9f770f78f5b9eef7654
Elli Cano <melongem64@gmail.com>
2025-04-18 19:05:57 -0700

    started adding native lsp config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- lazy.nvim install
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -11,5 +12,32 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- LSP config
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method("textDocument/completion") then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.diagnostic.config({ virtual_text = true })
+
+vim.lsp.config("*", {
+ 	capabilities = {
+ 		textDocument = {
+ 			semanticTokens = {
+ 				multilineTokenSupport = true,
+ 			},
+ 		},
+ 	},
+ 	root_markers = { ".git" },
+ })
+
+
+-- required files, plugins, and LSPs 
 require("vim-options")
 require("lazy").setup("plugins")
+
+vim.lsp.enable("lua_ls")
