e1ff4697121efc13798a61579b5d9d18af90b26c
Elli Cano <melongem64@gmail.com>
2025-04-18 19:58:49 -0700

    reversed all conflicting changes

--- init.lua
+++ init.lua
@@ -11,38 +11,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-<<<<<<< HEAD
--- LSP config
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method("textDocument/completion") then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
-    end
-  end,
-})
-
-vim.diagnostic.config({ virtual_text = true })
-
-vim.lsp.config("*", {
- 	capabilities = {
- 		textDocument = {
- 			semanticTokens = {
- 				multilineTokenSupport = true,
- 			},
- 		},
- 	},
- 	root_markers = { ".git" },
- })
-
-
--- required files, plugins, and LSPs 
 require("vim-options")
 require("lazy").setup("plugins")
-
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("ts_ls")
-=======
-require("vim-options")
-require("lazy").setup("plugins")
->>>>>>> parent of 9b53c40 (started adding native lsp config)
--- lsp/ts_ls.lua
+++
@@ -1,8 +0,0 @@
-vim.lsp.config("ts_ls", {
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  init_options = {
-    hostInfo = "neovim",
-  },
-  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
-})
