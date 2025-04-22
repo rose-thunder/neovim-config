af316d389c453c982d619e9911c5e0b4475352d2
Elli Cano <melongem64@gmail.com>
2025-04-22 15:13:33 -0700

    added cloak.nvim

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,18 @@
+return {
+  "laytan/cloak.nvim",
+  config = function()
+    local config = require("cloak")
+    config.setup({
+      enabled = true,
+      try_all_patterns = true,
+      cloak_on_leave = true,
+      patterns = {
+        {
+          file_pattern = ".env*",
+          cloak_pattern = "=.+",
+          replace = nil,
+        },
+      },
+    })
+  end,
+}
