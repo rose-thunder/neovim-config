0c9c69f2f829125a1cd1d0c481519a068fe4f626
Elli Cano <melongem64@gmail.com>
2025-07-27 17:37:01 -0500

    removed old lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,59 +14,15 @@ return {
           "ts_ls",
           "html",
           "cssls",
-          "pylsp",
+          "pyright",
           "rust_analyzer",
           "gopls",
+          "clangd",
         },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.pylsp.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        diagnostics = true,
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true,
-            },
-          },
-        },
-      })
-      vim.diagnostic.config({ virtual_text = true })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
   },
 }
