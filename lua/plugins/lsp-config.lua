a4bdf6ff8e54313fe6c11fe786a042c2ee172115
Elli Cano <melongem64@gmail.com>
2025-07-31 13:12:20 -0500

    changed lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,11 +15,71 @@ return {
           "pyright",
           "rust_analyzer",
           "clangd",
+          "html",
+          "omnisharp",
         },
       })
     end,
   },
   {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
     "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("rust_analyzer")
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("html")
+      vim.lsp.enable("omnisharp")
+      vim.lsp.enable("isort")
+      vim.lsp.enable("black")
+      vim.lsp.enable("stylua")
+      vim.lsp.enable("prettier")
+    end,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+        }),
+      })
+    end,
   },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,12 +10,8 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.golines,
       },
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
