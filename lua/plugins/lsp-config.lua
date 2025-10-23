624f06a59728e4c6c09cb01e8764261386564d9a
Elli Cano <melongem64@gmail.com>
2025-10-23 13:47:12 -0700

    removed swift lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,16 +27,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local config = require("lspconfig")
-      config.sourcekit.setup({
-        capabilities = {
-          workspace = {
-            didChangedWatchedFiles = {
-              dynamicRegistration = true,
-            },
-          },
-        },
-      })
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       vim.lsp.config("*", {
         capabilities = capabilities,
