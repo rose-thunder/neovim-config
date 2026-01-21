fad7dacf78d6bb05290f5961d3719aa7e5b23b4e
Elli Cano <melongem64@gmail.com>
2026-01-21 14:13:53 -0700

    added nvim-dap

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,9 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+
+    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<leader>dc", dap.continue, {})
+  end,
+}
