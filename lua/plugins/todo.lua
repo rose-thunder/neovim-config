e6a8e36a51ffcd8c8b1a223afefc70437ed8611e
Elli Cano <melongem64@gmail.com>
2026-09-12 09:35:10 -0700

    added keymaps for todo comments plugin

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,1 +1,10 @@
-require("todo-comments").setup({})
+local todo = require("todo-comments")
+todo.setup({})
+
+vim.keymap.set("n", "]t", function()
+    todo.jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+    todo.jump_prev()
+end, { desc = "Previous todo comment" })
